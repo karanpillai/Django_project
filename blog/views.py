36fb7c5419d2074be5fe10175eba7a844d5d7813
karanpillai <karanpillai721@gmail.com>
2020-09-27 18:24:47 +0530

    Blog Post Created

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render,HttpResponse
 
+posts = [
+    {
+        'author':'Karan',
+        'title': 'Blog post 1',
+        'description':'My first blog',
+        'date': 'September 27,2020'
+    },
+    {
+        'author':'Arjun',
+        'title': 'Blog post 2',
+        'description':'My Second blog',
+        'date': 'September 29,2020'
+    }
+]
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Blog App Created</h1>")
+    context = {
+        'posts' : posts
+    }
+    return render(request,'home.html',context)
+
+def about(request):
+    return render(request,'about.html',{"title":"About"})
